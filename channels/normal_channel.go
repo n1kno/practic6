8a138b64079aaf0e8acb5e0a558f02fe89143769
n1kno <125561616+n1kno@users.noreply.github.com>
2026-05-01 13:52:17 +0400

    Update normal_channel.go

--- channels/normal_channel.go
+++ channels/normal_channel.go
@@ -1,1 +1,39 @@
+package main
 
+import (
+	"fmt"
+	"time"
+)
+
+func NormalChannelClose() {
+	ch := make(chan int)
+	close(ch)
+	fmt.Println("канал закрыт")
+}
+
+func NormalChannelSendBlock() {
+	ch := make(chan int)
+	go func() {
+		time.Sleep(2 * time.Second)
+		<-ch
+		fmt.Println("данные получены")
+	}()
+	fmt.Println("отправка (блокировка на 2 сек)...")
+	ch <- 42
+	fmt.Println("отправка завершена")
+}
+
+func NormalChannelReceiveBlock() {
+	ch := make(chan int)
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch <- 100
+	}()
+	fmt.Println("чтение (блокировка на 2 сек)...")
+	val := <-ch
+	fmt.Println("получено:", val)
+}
+
+func main() {
+	NormalChannelSendBlock()
+}
