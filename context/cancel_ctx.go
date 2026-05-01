282a87925b233f5ec91d9c101c15a2245f504d4a
n1kno <125561616+n1kno@users.noreply.github.com>
2026-05-01 13:52:53 +0400

    Update cancel_ctx.go

--- context/cancel_ctx.go
+++ context/cancel_ctx.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		select {
+		case <-time.After(2 * time.Second):
+			fmt.Println("goroutine: done")
+		case <-ctx.Done():
+			fmt.Println("goroutine: cancelled, err:", ctx.Err())
+		}
+	}()
+
+	time.Sleep(1 * time.Second)
+	cancel()
+	time.Sleep(500 * time.Millisecond)
+}
