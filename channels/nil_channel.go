eda6cf15e3679598d83505e2be1f8d5fe59ad1a4
n1kno <125561616+n1kno@users.noreply.github.com>
2026-05-01 13:51:11 +0400

    Update nil_channel.go

--- channels/nil_channel.go
+++ channels/nil_channel.go
@@ -1,1 +1,28 @@
+package main
 
+import "fmt"
+
+func NilChannelClose() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("panic:", r)
+		}
+	}()
+	var ch chan int
+	close(ch)
+}
+
+func NilChannelSend() {
+	var ch chan int
+	ch <- 42 // deadlock
+}
+
+func NilChannelReceive() {
+	var ch chan int
+	<-ch 
+}
+
+func main() {
+
+	fmt.Println("раскомментируйте нужную функцию для демонстрации")
+}
