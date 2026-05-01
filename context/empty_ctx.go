45d5e844ff7b2805d5a0b6e5efd3028288f6aad2
n1kno <125561616+n1kno@users.noreply.github.com>
2026-05-01 13:52:39 +0400

    Update empty_ctx.go

--- context/empty_ctx.go
+++ context/empty_ctx.go
@@ -1,1 +1,14 @@
+package main
 
+import (
+	"context"
+	"fmt"
+)
+
+func main() {
+	bg := context.Background()
+	todo := context.TODO()
+
+	fmt.Printf("Background: %T, Done() = %v\n", bg, bg.Done())
+	fmt.Printf("TODO: %T, Done() = %v\n", todo, todo.Done())
+}
