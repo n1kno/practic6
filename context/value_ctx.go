dbf88e99dfec2577ebd6e3f786a17da357329c43
n1kno <125561616+n1kno@users.noreply.github.com>
2026-05-01 13:53:26 +0400

    Update value_ctx.go

--- context/value_ctx.go
+++ context/value_ctx.go
@@ -1,1 +1,23 @@
+package main
 
+import (
+	"context"
+	"fmt"
+)
+
+type keyType string
+
+const reqIDKey keyType = "requestID"
+
+func main() {
+	ctx := context.WithValue(context.Background(), reqIDKey, "req-12345")
+	process(ctx)
+}
+
+func process(ctx context.Context) {
+	if id, ok := ctx.Value(reqIDKey).(string); ok {
+		fmt.Println("request ID:", id)
+	} else {
+		fmt.Println("no request ID")
+	}
+}
