2f8bdcb68e554bc8342b397f4d6e410a5b03846b
n1kno <125561616+n1kno@users.noreply.github.com>
2026-05-01 13:51:46 +0400

    Update closed_channel.go

--- channels/closed_channel.go
+++ channels/closed_channel.go
@@ -1,1 +1,40 @@
+package main
 
+import "fmt"
+
+func ClosedChannelClose() {
+	ch := make(chan int)
+	close(ch)
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("panic:", r)
+		}
+	}()
+	close(ch) 
+}
+
+func ClosedChannelSend() {
+	ch := make(chan int)
+	close(ch)
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("panic:", r)
+		}
+	}()
+	ch <- 42 
+}
+
+func ClosedChannelReceive() {
+	ch := make(chan int, 1)
+	ch <- 100
+	close(ch)
+
+	val, ok := <-ch
+	fmt.Printf("1-е чтение: %v, ok=%v\n", val, ok) 
+	val, ok = <-ch
+	fmt.Printf("2-е чтение: %v, ok=%v\n", val, ok) 
+}
+
+func main() {
+	ClosedChannelReceive()
+}
